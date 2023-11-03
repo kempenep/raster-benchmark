44cd0ce6ace24c106e1b2acb21db1224509290ca
Pieter Kempeneers <kempepi@maggie.arolo>
2023-11-03 18:02:56 +0100

    added zonal statistics

--- pyjeo/zonal.py
+++ pyjeo/zonal.py
@@ -10,9 +10,9 @@ rasters = os.listdir(catalog)
 rasters = [r for r in rasters if r.endswith(('.TIF'))]
 rasters = [os.path.join(wd, catalog, r) for r in rasters]
 
-pts_path = os.path.join('data', 'vector', 'points.gpkg')
+buffers = os.path.join('data', 'vector', 'buffers.gpkg')
 
-v = pj.JimVect(pts_path)
+v = pj.JimVect(buffers)
 
 ### raster stack
 band_names = ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9"]
@@ -30,7 +30,7 @@ for i in range(10):
 
     output = '/vsimem/extracted' + str(i) + '.json'
     extracted = pj.geometry.extract(v, jim, output = output,
-                                    rule = ['allpoints'],
+                                    rule = ['mean'],
                                     oformat = 'GeoJSON')
 
     toc = timeit.default_timer()
@@ -39,9 +39,9 @@ for i in range(10):
     extracted.io.close()
 
 
-df = {'task': ['extract-points'] * 10, 'package': ['pyjeo'] * 10,
+df = {'task': ['zonal'] * 10, 'package': ['pyjeo'] * 10,
       'time': t_list}
 df = pd.DataFrame.from_dict(df)
 if not os.path.isdir('results'): os.mkdir('results')
-savepath = os.path.join('results', 'extract-points-pyjeo.csv')
+savepath = os.path.join('results', 'zonal-pyjeo.csv')
 df.to_csv(savepath, index = False, decimal = ',', sep = ';')
