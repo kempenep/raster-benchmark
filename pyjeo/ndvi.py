500a0b67be75dce320a03811e18dccac91e668a0
Pieter Kempeneers <kempepi@maggie.arolo>
2023-11-03 16:40:08 +0100

    added ndvi and adapted load

--- pyjeo/ndvi.py
+++ pyjeo/ndvi.py
@@ -12,23 +12,26 @@ rasters = [os.path.join(wd, catalog, r) for r in rasters]
 ### raster stack
 band_names = ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9"]
 
-### load
+jim = pj.Jim(rasters[5])
+jim.geometry.stackBand(pj.Jim(rasters[6]))
+jim.properties.setDimension(['red', 'nir'], 'band')
+#todo: check if needed
+jim.pixops.convert('GDT_Float32')
+
+### extract
 
 t_list = [None] * 10
 for i in range(10):
     tic = timeit.default_timer()
 
-    jim = pj.Jim()
-    for i, path in enumerate(rasters):
-        jim.geometry.stackBand(pj.Jim(path))
-    jim.properties.setDimension(band_names, 'band')
+    ndvi = pj.pixops.NDVI(jim, red = 'red', nir = 'nir')
 
     toc = timeit.default_timer()
     t_list[i] = round(toc - tic, 2)
 
 
-df = {'task': ['load'] * 10, 'package': ['pyjeo'] * 10, 'time': t_list}
+df = {'task': ['ndvi'] * 10, 'package': ['pyjeo'] * 10, 'time': t_list}
 df = pd.DataFrame.from_dict(df)
 if not os.path.isdir('results'): os.mkdir('results')
-savepath = os.path.join('results', 'load-pyjeo.csv')
+savepath = os.path.join('results', 'ndvi-pyjeo.csv')
 df.to_csv(savepath, index = False, decimal = ',', sep = ';')
